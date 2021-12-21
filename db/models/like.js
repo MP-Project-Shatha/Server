d7cdb8726cd4b15f8e857e30269eef7becd47d3d
ShathaAlmansour <>
2021-12-21 14:34:30 +0300

    Schema

---
+++ db/models/like.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const like = new mongoose.Schema({
+  like: { type: Boolean, default: false },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Exercises" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose.model("Like", like);
---
+++ db/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, require: true },
+    sex: { type: String },
+    weight: { type: String },
+    height: { type: String },
+    wrist: { type: String },
+    waist: { type: String },
+    chest: { type: String },
+    thigh: { type: String },
+    pelvis: { type: String },
+    ankle: { type: String },
+    armdiameter: { type: String },
+    musclediameter: { type: String },
+    fatpercentage: { type: String },
+    bonepercentage: { type: String },
+    active: { type: String },
+    eating: { type: String },
+
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+  },
+  timestamp(Date)
+);
+
+module.exports = mongoose.model("User", user);
