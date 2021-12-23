b2600a244a6319a0382a101140d1c761d6abe1fd
ShathaAlmansour <>
2021-12-23 11:11:47 +0300

    register

--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const userRoute = express.Router();
 
-
-const {
-  resgister,
-  activate,
-  login
-} = require("./../controllers/user");
+const { register, activate, login } = require("./../controllers/user");
 const authentication = require("./../middleware/authentication");
 
-
-
-userRoute.post("/resgister", resgister);
-userRoute.get('/activate/:token', activate);
+userRoute.post("/register", register);
+userRoute.get("/activate/:token", activate);
 userRoute.post("/login", login);
 // userRoute.post('/forgott', forgotPassword);
 // userRoute.get('/forgot/:token', gotoReset);
