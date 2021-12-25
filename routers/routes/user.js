5d81145805caa4265ed5f0c00c772d2d6a6b9d09
ShathaAlmansour <>
2021-12-25 22:18:56 +0300

    profile

--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const userRoute = express.Router();
 
-const { register, activate, login, addInfo, getInfo,forgotPassword,gotoReset,resetPassword } = require("./../controllers/user");
+const {
+  register,
+  activate,
+  login,
+  addInfo,
+  getInfo,
+  forgotPassword,
+  gotoReset,
+  resetPassword,
+  findUserByEmail,
+  editFullName,
+  deleteUser,
+} = require("./../controllers/user");
 const authentication = require("./../middleware/authentication");
 
 userRoute.post("/register", register);
@@ -9,8 +21,12 @@ userRoute.get("/activate/:token", activate);
 userRoute.post("/login", login);
 userRoute.post("/addInfo", addInfo);
 userRoute.get("/getInfo/:id", getInfo);
-userRoute.post('/forgott', forgotPassword);
-userRoute.get('/forgot/:token', gotoReset);
-userRoute.post('/reset/:id', resetPassword);
+userRoute.post("/forgott", forgotPassword);
+userRoute.get("/forgot/:token", gotoReset);
+userRoute.post("/reset/:id", resetPassword);
+// 
+userRoute.get("/email/:email", findUserByEmail);
+userRoute.put("/edit/:email", editFullName);
+userRoute.delete("/delete/:id", deleteUser);
 
 module.exports = userRoute;
